dbdd0a958ddcd82cc49517a60045b9a8f1b04d2e
DenArzik <denisthekwondist@gmail.com>
2018-04-22 13:08:35 +0300

    Ready to go

---
+++ Finder_2/source/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <vector>
+
+const static float minHessian = 400.f;
+const static float magicCoef = 3.f;
+
+void detect(const cv::Mat &im1, std::vector<cv::KeyPoint> &kp1, const cv::Mat &im2, std::vector<cv::KeyPoint> &kp2)
+{
+	cv::SurfFeatureDetector detector(minHessian);
+	detector.detect(im1, kp1);
+	detector.detect(im2, kp2);
+}
+void extract(const cv::Mat &im1, std::vector<cv::KeyPoint> &kp1, cv::Mat &dsc1, const cv::Mat &im2, std::vector<cv::KeyPoint> &kp2, cv::Mat &dsc2)
+{
+	cv::SurfDescriptorExtractor extractor;
+	extractor.compute(im1, kp1, dsc1);
+	extractor.compute(im2, kp2, dsc2);
+}
+void match(const cv::Mat &dsc1, const cv::Mat &dsc2, std::vector<cv::DMatch> &matches)
+{
+	cv::FlannBasedMatcher matcher;
+	matcher.match(dsc1, dsc2, matches);
+}
+
+int main()
+{
+	cv::VideoCapture cam(0);
+	if (!cam.isOpened())
+	{
+		std::cout << "NE RABOTAET KAMERA!\n";
+	}
+	cv::namedWindow("window", cv::WINDOW_AUTOSIZE);
+
+	cv::Mat img_obj = cv::imread("../../Finder_2/resources/obj.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat img_scene;
+
+		
+	while (true)
+	{
+		cam >> img_scene;
+		cv::resize(img_scene, img_scene, cv::Size(620, 480));
+		cv::flip(img_scene, img_scene, 1);
+
+		std::vector<cv::KeyPoint>  kp_obj, kp_scene;
+		detect(img_obj, kp_obj, img_scene, kp_scene);
+
+		cv::Mat dsc_obj, dsc_scene;
+		extract(img_obj, kp_obj, dsc_obj, img_scene, kp_scene, dsc_scene);
+			
+		if (dsc_scene.empty()) continue;
+		
+		std::vector<cv::DMatch> matches;
+		match(dsc_obj, dsc_scene, matches);
+
+		float maxDist = 0.f;
+		float minDist = 100.f;
+
+		for (int i = 0; i < dsc_obj.rows; ++i)
+		{
+			double dist = matches[i].distance;
+			if (dist < minDist) minDist = dist;
+			else if (dist > maxDist) maxDist = dist;
+		}
+
+		std::vector<cv::DMatch> goodMatches;
+
+		for (int i = 0; i < dsc_obj.rows; ++i)
+			if (matches[i].distance < magicCoef*minDist)
+				goodMatches.push_back(matches[i]);
+
+		cv::Mat imgMatches;
+		cv::drawMatches(img_obj, kp_obj, img_scene, kp_scene, goodMatches, imgMatches, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+		
+		std::vector<cv::Point2f> obj;
+		std::vector<cv::Point2f> scene;
+
+		for (int i(0); i < goodMatches.size(); ++i)
+		{
+			obj.push_back(kp_obj[goodMatches[i].queryIdx].pt);
+			scene.push_back(kp_scene[goodMatches[i].trainIdx].pt);
+		}
+		cv::Mat H;
+		try
+		{
+			H = cv::findHomography(obj, scene, cv::FM_RANSAC);
+		}
+		catch (...)
+		{
+			continue;
+		}
+	
+		std::vector<cv::Point2f> obj_corners(4), scene_corners(4);
+
+		obj_corners[0] = cv::Point(0, 0);
+		obj_corners[0] = cv::Point(img_obj.cols, 0);
+		obj_corners[0] = cv::Point(img_obj.cols, img_obj.rows);
+		obj_corners[0] = cv::Point(0, img_obj.rows);
+
+		cv::perspectiveTransform(obj_corners, scene_corners, H);
+
+		cv::line(imgMatches, scene_corners[0] + cv::Point2f(img_obj.cols, 0), scene_corners[1] + cv::Point2f(img_obj.cols, 0), cv::Scalar(0, 255, 0), 4);
+		cv::line(imgMatches, scene_corners[1] + cv::Point2f(img_obj.cols, 0), scene_corners[2] + cv::Point2f(img_obj.cols, 0), cv::Scalar(0, 255, 0), 4);
+		cv::line(imgMatches, scene_corners[2] + cv::Point2f(img_obj.cols, 0), scene_corners[3] + cv::Point2f(img_obj.cols, 0), cv::Scalar(0, 255, 0), 4);
+		cv::line(imgMatches, scene_corners[3] + cv::Point2f(img_obj.cols, 0), scene_corners[0] + cv::Point2f(img_obj.cols, 0), cv::Scalar(0, 255, 0), 4);
+
+
+		cv::imshow("window", imgMatches);
+		cv::waitKey(30);
+	}
+
+	
+	std::cin.get();
+	return 0;
+}
--- TemplateFinder/source/Main.cpp
+++ TemplateFinder/source/Main.cpp
@@ -1,38 +1,44 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
-#include <stdio.h>
 
 using namespace cv;
 
-/// Global Variables
-Mat img; Mat templ; Mat result;
-const char *image_window = "resources/dogs.jpg";
-const char *result_window = "resources/template.jpg";
 
-int match_method;
-int max_Trackbar = 5;
+const char *src_path("../../TemplateFinder/resources/dogs.jpg");
+const char *tmpl_path("../../TemplateFinder/resources/template.jpg");
 
-/// Function Headers
+const char *src_name("Source");
+const char *tmpl_name("Template");
+const char *result_name("Result");
+
+const int max_Trackbar(5);
+int match_method(0);
+
+Mat src_img;
+Mat tmpl_img;
+
+// Function Headers
 void MatchingMethod(int, void*);
+
 
 /** @function main */
 int main()
 {
-	/// Load image and template
-	img = imread(image_window);
-	templ = imread(result_window);
+	namedWindow(src_name, WINDOW_NORMAL);
+	namedWindow(tmpl_name, WINDOW_NORMAL);
+	namedWindow(result_name, WINDOW_NORMAL);
 
-	/// Create windows
-	namedWindow(image_window, WINDOW_NORMAL);
-	namedWindow(result_window, WINDOW_NORMAL);
+	resizeWindow(src_name, 600, 300);
+	resizeWindow(result_name, 600, 300);
 
-	resizeWindow(image_window, 1000, 500);
-	resizeWindow(result_window, 1000, 500);
+	src_img = imread(src_path);
+	tmpl_img = imread(tmpl_path);
 
-	/// Create Trackbar
+	imshow(tmpl_name, tmpl_img);
+
+	// Create Trackbar
 	const char *trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
-	createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod);
+	createTrackbar(trackbar_label, src_name, &match_method, max_Trackbar, MatchingMethod);
 
 	MatchingMethod(0, 0);
 
@@ -46,27 +52,25 @@ int main()
 */
 void MatchingMethod(int, void*)
 {
-	/// Source image to display
-	Mat img_display;
-	img.copyTo(img_display);
+	// Source image to display
+	Mat result_img;
+	src_img.copyTo(result_img);
 
-	/// Create the result matrix
-	int result_cols = img.cols - templ.cols + 1;
-	int result_rows = img.rows - templ.rows + 1;
+	// Create the result matrix
+	int result_cols = src_img.cols - tmpl_img.cols + 1;
+	int result_rows = src_img.rows - tmpl_img.rows + 1;
 
-	result.create(result_rows, result_cols, CV_32FC1);
+	// Do the Matching and Normalize
+	matchTemplate(src_img, tmpl_img, result_img, match_method);
+	normalize(result_img, result_img, 0, 1, NORM_MINMAX, -1, Mat());
 
-	/// Do the Matching and Normalize
-	matchTemplate(img, templ, result, match_method);
-	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
-
-	/// Localizing the best match with minMaxLoc
-	double minVal; double maxVal; Point minLoc; Point maxLoc;
+	// Localizing the best match with minMaxLoc
+	Point minLoc; Point maxLoc;
 	Point matchLoc;
 
-	minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+	minMaxLoc(result_img, 0,0, &minLoc, &maxLoc, Mat());
 
-	/// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+	// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
 	if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
 	{
 		matchLoc = minLoc;
@@ -76,12 +80,14 @@ void MatchingMethod(int, void*)
 		matchLoc = maxLoc;
 	}
 
-	/// Show me what you got
-	rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
-	rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+	src_img = imread(src_path);
 
-	imshow(image_window, img_display);
-	imshow(result_window, result);
+	// Show me what you got
+	rectangle(src_img, matchLoc, Point(matchLoc.x + tmpl_img.cols, matchLoc.y + tmpl_img.rows), Scalar::all(0), 2, 8, 0);
+	rectangle(result_img, matchLoc, Point(matchLoc.x + tmpl_img.cols, matchLoc.y + tmpl_img.rows), Scalar::all(0), 2, 8, 0);
+	
+	imshow(src_name, src_img);
+	imshow(result_name, result_img);
 
 	return;
 }
